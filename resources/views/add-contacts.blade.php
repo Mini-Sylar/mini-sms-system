f1136c5975caa3754e9aa23cfb6915bed827d522
Mini-Sylar <minisylar3@gmail.com>
2022-11-03 16:10:34 +0000

    added new contact component

--- resources/views/add-contacts.blade.php
+++ resources/views/add-contacts.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="{{asset('css/contacts.css')}}" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
@@ -12,7 +13,33 @@
 
     <main>
         <section>
-            
+            {{-- Create table with heading name, number and actions --}}
+            <div class="main-container">
+                {{-- Heading --}}
+                <h2>Contact List</h2>
+                <div class="table-container">
+                      <table class="table">
+                <tr>
+                    <th>Name</th>
+                    <th>Phone Number</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>John Doe</td>
+                    <td>02xxxxxxxxx</td>
+                    <td class="actions">
+                        <a href="">Update</a>
+                        <a href="">Delete</a>
+                    </td>
+                </tr>
+            </table>
+                </div>
+                
+            {{-- Create Contact Button Here --}}
+            <div class="create-contact">
+                <a href="#">Create Contact</a>
+            </div>
+          
         </section>
     </main>
 </body>
---
+++ resources/views/components/new-contact.blade.php
@@ -0,0 +1,14 @@
+<aside>
+    <h2>New Contact</h2>
+    <div class="add-new-contact">
+         <form method="POST" action="/contacts">
+        @csrf
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" value="">
+        <label for="phone">Phone</label>
+        <input type="text" name="phone" id="phone" value="">
+        <button type="submit">Add Contact</button>
+    </form>
+    </div>
+   
+</aside>
