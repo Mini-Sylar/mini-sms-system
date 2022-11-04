5415bff09bea2ad6fefdfeb27b7b50a96e151f2f
Mini-Sylar <minisylar3@gmail.com>
2022-11-04 09:39:24 +0000

    make sure all forms are filled

--- resources/views/components/new-contact.blade.php
+++ resources/views/components/new-contact.blade.php
@@ -2,7 +2,7 @@
 
 <aside class="new-contact-aside">
     <h2>New Contact</h2>
-     <div id="app">
+     <div id="app" class="flash-message"> 
         @include('flash-message')
 
 
@@ -13,11 +13,17 @@
         <form method="POST" action="/contacts">
         @csrf
         <label for="name">Name</label>
-        <input type="text" name="name" id="name" value="">
+        <input type="text" name="name" id="name" value="" required>
         <label for="phone">Phone</label>
-        <input type="text" name="phone" id="phone" value="">
+        <input type="text" name="phone" id="phone" value="" required>
         <button type="submit" class="add-contact-button">Add Contact</button>
     </form>
     </div>
-   
+    <script>
+        const flash_message = document.querySelector('.flash-message')
+        // hide flash message after 4 seconds
+        setTimeout(() => {
+            flash_message.style.visibility = 'hidden'
+        }, 4000)
+    </script>
 </aside>
