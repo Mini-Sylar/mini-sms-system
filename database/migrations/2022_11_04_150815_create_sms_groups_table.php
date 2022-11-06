378c4fa8a71e2af0a1cdbd1b88a7f69f8d01c8f1
Mini-Sylar <minisylar3@gmail.com>
2022-11-06 11:29:55 +0000

    cleaned migrations

--- database/migrations/2022_11_04_150815_create_sms_groups_table.php
+++ database/migrations/2022_11_04_150815_create_sms_groups_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sms_groups', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('group_name');
+            $table->longText('contact_number')->nullable();
+            $table->string('created_by');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sms_group');
     }
 };
