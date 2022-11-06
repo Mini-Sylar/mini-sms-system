fe4286b81d67074cd89d5a176289f4ff3b73709e
Mini-Sylar <minisylar3@gmail.com>
2022-11-06 21:33:36 +0000

    Added data sending

--- app/Http/Controllers/QuickMessageController.php
+++ app/Http/Controllers/QuickMessageController.php
@@ -32,17 +32,17 @@ class QuickMessageController extends Controller
             'schedule_date' => ''
         ];
         // Send Message Here
-        $ch = curl_init();
-        $headers = array();
-        $headers[] = "Content-Type: application/json";
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        $result = curl_exec($ch);
-        $result = json_decode($result, TRUE);
-        curl_close($ch);
+        // $ch = curl_init();
+        // $headers = array();
+        // $headers[] = "Content-Type: application/json";
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        // curl_setopt($ch, CURLOPT_URL, $url);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        // $result = curl_exec($ch);
+        // $result = json_decode($result, TRUE);
+        // curl_close($ch);
         return back()->with('success', 'Message sent successfully');
     }
 }
